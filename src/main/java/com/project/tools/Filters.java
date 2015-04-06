edc74711aeedb63a225cfcf0f448bd7fbdbbec64
Robert <Robert@LenovoG580>
2015-04-06 12:08:19 +0200

    Nowy filtr konwolucyjny (rozmycie pionowe)

--- src/main/java/com/project/tools/Filters.java
+++ src/main/java/com/project/tools/Filters.java
@@ -13,7 +13,91 @@ public class Filters {
      public double ww[]=new double[3];
      private int pixels[] = new int[3];
     
-    // Filtr sepia//
+// Filtr konwolucyjny//
+    public WritableRaster convolutionalFilter(WritableRaster raster) {
+        int matrix[][] = {{0, 3, 0}, {0, 1, 0}, {0, 3, 0}};
+
+        for (int i = 0; i < raster.getWidth(); i++) {
+            for (int j = 0; j < raster.getHeight(); j++) {
+                int tmpPixels[][][] = new int[3][3][3];
+
+                if (i - 1 > 0 && j - 1 > 0) {
+                    raster.getPixel(i - 1, j - 1, tmpPixels[0][0]);
+                }
+                if (i - 1 > 0) {
+                    raster.getPixel(i - 1, j, tmpPixels[0][1]);
+                }
+                if (i - 1 > 0 && j + 1 < raster.getHeight()) {
+                    raster.getPixel(i - 1, j + 1, tmpPixels[0][2]);
+                }
+
+                if (j - 1 > 0) {
+                    raster.getPixel(i, j - 1, tmpPixels[1][0]);
+                }
+                raster.getPixel(i, j, tmpPixels[1][1]);
+                if (j + 1 < raster.getHeight()) {
+                    raster.getPixel(i, j + 1, tmpPixels[1][2]);
+                }
+
+                if (i + 1 < raster.getWidth() && j - 1 > 0) {
+                    raster.getPixel(i + 1, j - 1, tmpPixels[2][0]);
+                }
+                if (i + 1 < raster.getWidth()) {
+                    raster.getPixel(i + 1, j, tmpPixels[2][1]);
+                }
+                if (i + 1 < raster.getWidth() && j + 1 < raster.getHeight()) {
+                    raster.getPixel(i + 1, j + 1, tmpPixels[2][2]);
+                }
+
+                int weightSum = matrix[0][0] + matrix[0][1] + matrix[0][2]
+                        + matrix[1][0] + matrix[1][1] + matrix[1][2]
+                        + matrix[2][0] + matrix[2][1] + matrix[2][2];
+
+                int valueRedSum = 0,
+                        valueGreenSum = 0,
+                        valueBlueSum = 0;
+
+                valueRedSum = tmpPixels[0][0][0] * matrix[0][0]
+                        + tmpPixels[0][1][0] * matrix[0][1]
+                        + tmpPixels[0][2][0] * matrix[0][2]
+                        + tmpPixels[1][0][0] * matrix[1][0]
+                        + tmpPixels[1][1][0] * matrix[1][1]
+                        + tmpPixels[1][2][0] * matrix[1][2]
+                        + tmpPixels[2][0][0] * matrix[2][0]
+                        + tmpPixels[2][1][0] * matrix[2][1]
+                        + tmpPixels[2][2][0] * matrix[2][2];
+
+                valueGreenSum = tmpPixels[0][0][1] * matrix[0][0]
+                        + tmpPixels[0][1][1] * matrix[0][1]
+                        + tmpPixels[0][2][1] * matrix[0][2]
+                        + tmpPixels[1][0][1] * matrix[1][0]
+                        + tmpPixels[1][1][1] * matrix[1][1]
+                        + tmpPixels[1][2][1] * matrix[1][2]
+                        + tmpPixels[2][0][1] * matrix[2][0]
+                        + tmpPixels[2][1][1] * matrix[2][1]
+                        + tmpPixels[2][2][1] * matrix[2][2];
+
+                valueBlueSum = tmpPixels[0][0][2] * matrix[0][0]
+                        + tmpPixels[0][1][2] * matrix[0][1]
+                        + tmpPixels[0][2][2] * matrix[0][2]
+                        + tmpPixels[1][0][2] * matrix[1][0]
+                        + tmpPixels[1][1][2] * matrix[1][1]
+                        + tmpPixels[1][2][2] * matrix[1][2]
+                        + tmpPixels[2][0][2] * matrix[2][0]
+                        + tmpPixels[2][1][2] * matrix[2][1]
+                        + tmpPixels[2][2][2] * matrix[2][2];
+
+                ww[0] = valueRedSum / weightSum;
+                ww[1] = valueGreenSum / weightSum;
+                ww[2] = valueBlueSum / weightSum;
+
+                raster.setPixel(i, j, ww);
+            }
+        }
+        return raster;
+    }   
+     
+// Filtr sepia//
     public WritableRaster sepiaFilter(WritableRaster raster)
     {
         for(int i=0;i<raster.getWidth();i++)
