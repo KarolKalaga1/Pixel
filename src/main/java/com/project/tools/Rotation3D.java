f65b1583d74bec691e96b32e3d37fe18a8626851
Robert <Robert@LenovoG580>
2015-05-20 23:21:08 +0200

    Dodanie obracania obrazu w 3D (slidery)

--- src/main/java/com/project/tools/Rotation3D.java
+++ src/main/java/com/project/tools/Rotation3D.java
@@ -5,12 +5,15 @@
  */
 package com.project.tools;
 
+import com.project.enums.ImageProcessEnum;
 import com.sun.j3d.utils.geometry.Box;
 import com.sun.j3d.utils.image.TextureLoader;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import java.awt.BorderLayout;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import javax.media.j3d.Alpha;
 import javax.media.j3d.Appearance;
@@ -22,9 +25,17 @@ import javax.media.j3d.Texture;
 import javax.media.j3d.TextureAttributes;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
+import org.pushingpixels.substance.api.SubstanceLookAndFeel;
+import org.pushingpixels.substance.api.skin.GraphiteAquaSkin;
 
 /**
  *
@@ -32,28 +43,99 @@ import javax.vecmath.Vector3d;
  */
 public class Rotation3D extends JFrame {
 
+    BufferedImage bi;
+    JButton buttonSave;
+    JSlider sliderX;
+    JSlider sliderY;
+    RotationAction rotationAction = new RotationAction();
+    JPanel imagePanel;
+        
     public Rotation3D(BufferedImage bi) throws HeadlessException {
-
+        this.bi = bi;
+        setUndecorated(true);
+        getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+        SubstanceLookAndFeel.setSkin(new GraphiteAquaSkin());//new GraphiteAquaSkin()//new GraphiteGlassSkin()//new GraphiteSkin() //new TwilightSkin()
+        SubstanceLookAndFeel.setSkin("org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel");
+        
+        setTitle("Rotation 3D");
         setLayout(new BorderLayout());
         setSize(500, 500);
         
+        setResizable(false);
+        setVisible(true);
+        init();
+    }
+    
+    public void init(){
+        imagePanel = new JPanel();
+        imagePanel.setVisible(true);
+        imagePanel.setSize(100, 100);
+        
+        buttonSave   = new JButton("Save");
+        sliderX = new JSlider(-100, 100, 0);
+        sliderY = new JSlider(JSlider.VERTICAL, -100, 100, 0);
+        
+        buttonSave.addActionListener(rotationAction);
+        sliderX.addChangeListener(rotationAction);
+        sliderY.addChangeListener(rotationAction);
+        
+        add(imagePanel,BorderLayout.CENTER);
+        add(sliderX,BorderLayout.NORTH);
+        add(sliderY,BorderLayout.EAST);
+        add(buttonSave,BorderLayout.SOUTH);
+        
+        paint(0.0, 0.0);
+    }     
+    
+    public class RotationAction implements ActionListener,ChangeListener
+    {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == buttonSave)
+            {
+                //Zapis do pliku
+            }
+        }
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            if(e.getSource() == sliderX)
+            {
+               paint(0.6, 0.0);
+            }
+            
+            if(e.getSource() == sliderY)
+            {
+               paint(0.0, 0.6);
+            }
+        }
+    }
+     
+    public void paint(double angleX, double angleY){
         GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
         Canvas3D canvas = new Canvas3D(config);
-        add("Center", canvas);
+        imagePanel.add("Center", canvas);
 
         TransformGroup tg_content = getScene(bi);
-
+        
+        Transform3D rotation = new Transform3D();
+        
+        rotation.rotY(angleY);
+        rotation.rotX(angleX);
+        
+        tg_content.setTransform(rotation);
+        
         BranchGroup content = new BranchGroup();
 
         content.addChild(tg_content);
         content.compile();
 
         SimpleUniverse universe = new SimpleUniverse(canvas);
-        //
-        Transform3D move = lookTowardsOriginFrom(new Point3d(0, 0, -3));
-        universe.getViewingPlatform().getViewPlatformTransform().setTransform(move);
+        
+        universe.getViewingPlatform().setNominalViewingTransform();
         universe.addBranchGraph(content);
-
+        
         setVisible(true);
     }
 
@@ -84,13 +166,5 @@ public class Rotation3D extends JFrame {
         group.addChild(rotator);
 
         return group;
-    }
-
-    public Transform3D lookTowardsOriginFrom(Point3d point) {
-        Transform3D move = new Transform3D();
-        Vector3d up = new Vector3d(point.x, point.y + 1, point.z);
-        move.lookAt(point, new Point3d(1.0d, 0.0d, 0.0d), up);
-
-        return move;
     }
 }
